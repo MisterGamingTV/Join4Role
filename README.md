693a475ccc40edfd76a5eac89907066d6d36c5ec
MisterGamingTV <jojo.schorn@outlook.de>
2022-04-04 17:41:12 +0200

    First version - NOT WORKING ATM
    Co-authored-by: Markus <Zedruc@users.noreply.github.com>
    Co-authored-by: pauel <ppauel@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Join4Role
 
 Just a little Discordbot to assign roles to members in a Voice Channel
+
+## Setup
+To setup the Bot you basically just need to fill in all informations in the `config.json` file. Here's a little explanation what the different informations are for:
+
+**Token:**
+    That your Bot can connect to Discord you need to get your Token. Just create a Bot on the [Discord Developer Portal](https://discordapp.com/developers/applications/me) and copy the Token.
+
+**Role:**
+    `role` is the role ID you want to assign to the members if they join a Voice Channel. 
+
+**Ignore Channel**
+    ID of the Stage channel
+
+**Cooldown**
+    Cooldown for the bot to not reach the API limit of Discord. We recommend to set it larger than four seconds. Please note that the time must be given in miliseconds.
---
+++ index.js
@@ -0,0 +1,120 @@
+// Require important things
+const { Client, Intents } = require('discord.js');
+const config = require('./config.json');
+
+// config.cooldown HAS TO BE specified in milliseconds
+const cooldown = config.cooldown;
+var lastUserJoined;
+
+// Util
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+// Set all Intents
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+// Send message to console
+client.on('ready', () => {
+	console.log(`[LOG] Connected to Discord with client ${client.user.tag}`);
+	client.user.setActivity(`all Voice Channels 👀`, { type: 'WATCHING' }); //Set Activity
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+	console.log('EVENT FIRED');
+	let newChannel = newState.voiceChannel;
+	let oldChannel = oldState.voiceChannel;
+
+	const member = newState.member;
+
+	// ping role
+	const roleID = config.role;
+	const role = newState.guild.roles.cache.find(r => r.id === roleID); // just use the VoiceState object itself
+
+	if (!role) throw new Error(`Wasn't able to find role (${roleID}) `)
+
+	if(newChannel?.id) { // false if id is undefined
+		console.log('[LOG] Joined channel');
+		// connect event
+		if (newChannel.id != config.ignore_channel) { // id must exist at this point
+			// escape ignored channel
+			// add role
+
+			console.log('[LOG] Joined not ignored channel');
+
+			// if timespan between user joining and right now is less than the cooldown, wait it out (custom sleep function);
+			/* if (Date.now() - lastUserJoined < cooldown) {
+				console.log('[LOG]  Cooldown triggered');
+				await sleep(cooldown); // should make script wait for specified cooldown
+			} */
+
+			try {
+				member.roles.add(role.id);
+				lastUserJoined = Date.now();
+				console.log('[LOG] Added role');
+			} catch (error) {
+				console.log(`[ERROR] Wasn't able to add role \`${role.name}\` to member (${member.user.tag})`);
+			}
+			
+		} else return; // channel IS ignored channel
+	} 
+	// disconnect event
+	else if (oldChannel?.id) { // fehler hier?
+		console.log('[LOG] Disconnect event?');
+		if (oldChannel.id === config.ignore_channel) return; // return if previous channel was ignored 
+
+		try {
+			member.roles.remove(role.id);
+			console.log('[LOG] Removed role');
+		} catch (error) {
+			console.log(`[ERROR] Wasn't able to remove role \`${role.name}\` from member (${member.user.tag})`);
+		}
+	}
+	else {
+		console.log('Last else');
+	}
+});
+
+// Connect client to Discord
+client.login(config.token);
+
+
+
+
+
+// old try
+
+
+/* client.on('voiceStateUpdate', (oldState, newState) => {
+	let newChannel = newState.voiceChannelID;
+	let oldChannel = oldState.voiceChannelID;
+
+	// poor man's debug
+	console.log(`[DEBUG] newChannel: ${newChannel.id}`);
+	console.log(`[DEBUG] oldChannel: ${oldChannel.id}`);
+
+	//Check if channel is Stage
+	if (newChannel !== oldChannel) {
+
+		let roleId = config.role;
+		let role = newState.guild.roles.cache.find(r => r.id === roleId);
+		if (!role) throw new Error(`[ERROR] Couldn't find role to assign: ${config.role}`); // throw exits
+
+		if (newChannel.id != config.ignore_channel) { // Rolle wird geadded wenn: neuer Kanal =! Voice
+			// user joined channel
+			try {
+				newState.member.roles.add(role);
+			} catch (error) {
+				console.log('[LOG] Failed to add role to user');
+			}
+
+		} else if (newChannel.id !== config.ignore_channel) { // die rolle wird dann aber nicht removed, wenn man aus einem vc in die stage geht?
+			// Execute this if user left 
+			try {
+				newState.member.roles.remove(role);
+			} catch (error) {
+				console.log(`[LOG] Failed to remove role \`${config.role}\` from user`);
+			}
+			
+
+		}
+	}
+}); */
